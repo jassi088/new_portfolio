7da2236d142606dde6c4a6e065626a87e473a555
jaspreet-bcoder <jaspreetbcoder@gmail.com>
2024-07-17 11:58:10 +0530

    added location to experiences data

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -4,11 +4,13 @@ export const experiences = [
     title: "Full stack Developer",
     company: "Bcoder castle pvt. ltd.",
     duration: "(July 2023 - Present)",
+    location: "Sahibzada Ajit Singh Nagar, Punjab, India",
   },
   {
     id: 2,
     title: "Mern stack Developer",
     company: "Omninos Technologies pvt. ltd.",
     duration: "(May 2023 - July 2023)",
+    location: "Sahibzada Ajit Singh Nagar, Punjab, India",
   },
 ];
