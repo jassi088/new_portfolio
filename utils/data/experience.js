1570ce29f16ef68f01851293f6766602e50e6aa8
jaspreet-bcoder <jaspreetbcoder@gmail.com>
2024-07-17 11:49:00 +0530

    code

---
+++ utils/data/experience.js
@@ -0,0 +1,14 @@
+export const experiences = [
+  {
+    id: 1,
+    title: "Full stack Developer",
+    company: "Bcoder castle pvt. ltd.",
+    duration: "(July 2023 - Present)",
+  },
+  {
+    id: 2,
+    title: "Mern stack Developer",
+    company: "Omninos Technologies pvt. ltd.",
+    duration: "(May 2023 - July 2023)",
+  },
+];
